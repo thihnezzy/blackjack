63f2208f86bcec305572b065807eb8d2bb2d5fb5
Quoc Thinh <quoc_thinh.nguyen@insa-cvl.fr>
2022-12-20 21:39:31 +0100

    google verification

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from "react";
 import { Button, Container, Figure, Row, Col, ButtonGroup} from "react-bootstrap";
+import {Sound} from 'react-sound';
 import classes from "./Game.module.css";
 import Card from "./Card/Card";
 import GameControl from "./GameControl/GameControl";
